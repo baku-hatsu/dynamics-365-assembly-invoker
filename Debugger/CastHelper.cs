1dd415794203a4451e521a647fa05e76389d6097
Paulius Piečius <paulius_8@live.com>
2022-10-23 02:45:54 +0200

    Fix bug where nullable type would throw an error while casting

--- Debugger/CastHelper.cs
+++ Debugger/CastHelper.cs
@@ -6,16 +6,20 @@ namespace Debugger
     {
         public static object Cast(Type type, string value)
         {
-            switch (type.Name)
+            switch (type)
             {
-                case nameof(Guid):
+                case Type guidType when guidType == typeof(Guid):
+                case Type nullableguidType when nullableguidType == typeof(Guid?):
                     return Guid.TryParse(value, out Guid guid) ? guid : (Guid?)null;
 
-                case nameof(DateTime):
+                case Type dateTimeType when dateTimeType == typeof(DateTime):
                     return DateTime.TryParse(value, out DateTime dateTime) ? dateTime : (DateTime?)null;
 
-                case nameof(Boolean):
+                case Type boolType when boolType == typeof(bool):
                     return bool.TryParse(value, out bool boolean) ? boolean : (bool?)null;
+
+                case Type intType when intType == typeof(int):
+                    return int.TryParse(value, out int integer) ? integer : (int?)null;
 
                 default:
                     return Convert.ChangeType(value, type);
