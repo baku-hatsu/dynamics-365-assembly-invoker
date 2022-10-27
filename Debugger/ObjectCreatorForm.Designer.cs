f705b0731382644339fd311aa6a8c9b9c9ad2598
Paulius Piečius <paulius_8@live.com>
2022-10-27 22:59:34 +0200

    Add object creator form

---
+++ Debugger/ObjectCreatorForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Debugger
+{
+    partial class ObjectCreatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.object_data_grid_view = new System.Windows.Forms.DataGridView();
+            this.PropertyName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PropertyType = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.PropertyValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ok_button = new System.Windows.Forms.Button();
+            this.cancel_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.object_data_grid_view)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // object_data_grid_view
+            // 
+            this.object_data_grid_view.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.object_data_grid_view.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.object_data_grid_view.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.PropertyName,
+            this.PropertyType,
+            this.PropertyValue});
+            this.object_data_grid_view.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.object_data_grid_view.Location = new System.Drawing.Point(12, 12);
+            this.object_data_grid_view.Name = "object_data_grid_view";
+            this.object_data_grid_view.Size = new System.Drawing.Size(560, 208);
+            this.object_data_grid_view.TabIndex = 0;
+            this.object_data_grid_view.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.Object_data_grid_view_cell_mouse_click);
+            // 
+            // PropertyName
+            // 
+            this.PropertyName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.PropertyName.DataPropertyName = "PropertyName";
+            this.PropertyName.HeaderText = "Name";
+            this.PropertyName.Name = "PropertyName";
+            this.PropertyName.ToolTipText = "Name";
+            // 
+            // PropertyType
+            // 
+            this.PropertyType.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.PropertyType.DataPropertyName = "PropertyType";
+            this.PropertyType.HeaderText = "Type";
+            this.PropertyType.Name = "PropertyType";
+            this.PropertyType.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.PropertyType.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.PropertyType.ToolTipText = "Type";
+            // 
+            // PropertyValue
+            // 
+            this.PropertyValue.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.PropertyValue.DataPropertyName = "PropertyValue";
+            this.PropertyValue.HeaderText = "Value";
+            this.PropertyValue.Name = "PropertyValue";
+            this.PropertyValue.ToolTipText = "Value";
+            // 
+            // ok_button
+            // 
+            this.ok_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ok_button.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.ok_button.Location = new System.Drawing.Point(497, 226);
+            this.ok_button.Name = "ok_button";
+            this.ok_button.Size = new System.Drawing.Size(75, 23);
+            this.ok_button.TabIndex = 1;
+            this.ok_button.Text = "OK";
+            this.ok_button.UseVisualStyleBackColor = true;
+            this.ok_button.Click += new System.EventHandler(this.Ok_button_click);
+            // 
+            // cancel_button
+            // 
+            this.cancel_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancel_button.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancel_button.Location = new System.Drawing.Point(416, 226);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(75, 23);
+            this.cancel_button.TabIndex = 2;
+            this.cancel_button.Text = "Cancel";
+            this.cancel_button.UseVisualStyleBackColor = true;
+            // 
+            // ObjectCreatorForm
+            // 
+            this.AcceptButton = this.ok_button;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancel_button;
+            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.cancel_button);
+            this.Controls.Add(this.ok_button);
+            this.Controls.Add(this.object_data_grid_view);
+            this.MinimumSize = new System.Drawing.Size(600, 300);
+            this.Name = "ObjectCreatorForm";
+            this.Text = "Object creator";
+            ((System.ComponentModel.ISupportInitialize)(this.object_data_grid_view)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView object_data_grid_view;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PropertyName;
+        private System.Windows.Forms.DataGridViewComboBoxColumn PropertyType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PropertyValue;
+        private System.Windows.Forms.Button ok_button;
+        private System.Windows.Forms.Button cancel_button;
+    }
+}
---
+++ Debugger/ObjectCreatorForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Windows.Forms;
+
+namespace Debugger
+{
+    public partial class ObjectCreatorForm : Form
+    {
+        public object _result = null;
+
+        private const int NameIndex = 0;
+        private const int TypeIndex = 1;
+        private const int ValueIndex = 2;
+
+        public ObjectCreatorForm(ExpandoObject defaultValue = null)
+        {
+            InitializeComponent();
+            SetUpTypesComboBox(TypeIndex);
+
+            if (defaultValue != null)
+            {
+                SetUpDefaultValues(defaultValue);
+            }
+        }
+
+        private void SetUpDefaultValues(ExpandoObject existingExpandoObject)
+        {
+            foreach (KeyValuePair<string, object> property in existingExpandoObject)
+            {
+                var rowIndex = object_data_grid_view.Rows.Add();
+                object_data_grid_view.Rows[rowIndex].Cells[NameIndex].Value = property.Key;
+                object_data_grid_view.Rows[rowIndex].Cells[TypeIndex].Value = property.Value?.GetType();
+                object_data_grid_view.Rows[rowIndex].Cells[ValueIndex].Value = property.Value;
+            }
+        }
+
+        private void SetUpTypesComboBox(int columnIndex)
+        {
+            if (object_data_grid_view.Columns[columnIndex] is DataGridViewComboBoxColumn typeColumn)
+            {
+                typeColumn.DisplayMember = "Name";
+                typeColumn.ValueMember = "Value";
+                typeColumn.Items.Add(new { Name = typeof(object).FullName, Value = typeof(object) });
+                typeColumn.Items.Add(new { Name = typeof(Guid).FullName, Value = typeof(Guid) });
+                typeColumn.Items.Add(new { Name = typeof(string).FullName, Value = typeof(string) });
+                typeColumn.Items.Add(new { Name = typeof(int).FullName, Value = typeof(int) });
+                typeColumn.Items.Add(new { Name = typeof(bool).FullName, Value = typeof(bool) });
+                typeColumn.Items.Add(new { Name = typeof(DateTime).FullName, Value = typeof(DateTime) });
+            }
+        }
+
+        private void Ok_button_click(object sender, EventArgs e)
+        {
+            var result = new ExpandoObject();
+
+            for (int index = 0; index < object_data_grid_view.RowCount - 1; index++)
+            {
+                if (object_data_grid_view.Rows[index].Cells[TypeIndex].Value is Type type)
+                {
+                    var name = object_data_grid_view.Rows[index].Cells[NameIndex].Value as string;
+                    var value = object_data_grid_view.Rows[index].Cells[ValueIndex].Value;
+
+                    if (type.FullName.Equals(typeof(object).FullName))
+                    {
+                        result.AddProperty(name, value);
+                    }
+                    else
+                    {
+                        result.AddProperty(name, TypeHelper.Cast(type, value.ToString()));
+                    }
+                }
+            }
+
+            _result = result;
+        }
+
+        private void Object_data_grid_view_cell_mouse_click(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex > -1 && e.ColumnIndex == ValueIndex)
+            {
+                if (object_data_grid_view.Rows[e.RowIndex].Cells[TypeIndex].Value is Type item && item.FullName.Equals(typeof(object).FullName))
+                {
+                    var form = new ObjectCreatorForm(object_data_grid_view.Rows[e.RowIndex].Cells[ValueIndex].Value as ExpandoObject);
+
+                    var result = form.ShowDialog();
+
+                    if (result == DialogResult.OK)
+                    {
+                        object_data_grid_view.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = form._result;
+                    }
+                }
+            }
+        }
+    }
+}
